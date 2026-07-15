b495f6607d125786dcb18bf87e38e1d1b6960299
carson <carson.chambers@devrygreenhouses.com>
2026-07-15 09:53:26 -0700

    Just found out about Diffview File History, heck yeah

--- lua/pack/neogit.lua
+++ lua/pack/neogit.lua
@@ -14,3 +14,33 @@ vim.keymap.set("n", "<leader>gd", function()
     vim.cmd("DiffviewOpen")
   end
 end, { desc = "Diffview toggle" })
+
+-- all history
+vim.keymap.set("n", "<leader>gha", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewFileHistory")
+  end
+end, { desc = "Diffview toggle" })
+
+-- current file history
+vim.keymap.set("n", "<leader>ghc", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewFileHistory %")
+  end
+end, { desc = "Diffview toggle" })
+
+-- current section of current file history
+vim.keymap.set("v", "<leader>ghc", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd(":'<,'>DiffviewFileHistory %")
+  end
+end, { desc = "Diffview toggle" })
