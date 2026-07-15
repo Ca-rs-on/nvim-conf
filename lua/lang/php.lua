ea6650f8f972ab6e3d06b5c4453525965b4e82d8
carson <carson.chambers@devrygreenhouses.com>
2026-07-15 08:01:06 -0700

    its not an api key

--- lua/lang/php.lua
+++ lua/lang/php.lua
@@ -4,8 +4,7 @@ vim.lsp.config("intelephense", {
   root_markers = { "composer.json", ".git" },
   settings = {
     intelephense = {
-      -- e.g. files = { maxSize = 5000000 },  -- bump for large frameworks
-      licenceKey = os.getenv("INTELEPHENSE_API_KEY"),  -- only if you have premium (see below)
+      licenceKey = os.getenv("INTELEPHENSE_PRO_KEY"),  
     },
   },
 })
--- lua/pack/telescope.lua
+++ lua/pack/telescope.lua
@@ -2,14 +2,14 @@ vim.pack.add({
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/nvim-telescope/telescope.nvim'
 })
-
 telescope = require('telescope')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.grep_string, { desc = 'Search under cursor' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help' })
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope oldfiles' })
-vim.keymap.set('n', '<C-b>', builtin.lsp_references, { desc = 'Find usages' })
+vim.keymap.set('n', '<C-b>', function() builtin.lsp_implementations({ jump_type = "tab drop" }) end, { desc = 'Find usages' })
 vim.keymap.set('n', '<C-f>', builtin.current_buffer_fuzzy_find, { desc = 'Search file' })
 
