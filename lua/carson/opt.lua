7376812ce1d7774bdf31c7d9ea160ccb9fd7dc4a
carson <carson@nerdware.ca>
2026-07-20 15:30:26 -0700

    better tabbing

--- lua/carson/opt.lua
+++ lua/carson/opt.lua
@@ -3,6 +3,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
-vim.opt.incsearch = true
 vim.o.splitright = true
 vim.o.exrc = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
