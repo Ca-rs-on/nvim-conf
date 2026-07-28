ade67bf76025b883c3b7c667c2f4ae711d5115f8
carson <carson.chambers@devrygreenhouses.com>
2026-07-28 13:02:32 -0700

    Allows for convenient project jumping w/o closing nvim

--- lua/carson/init.lua
+++ lua/carson/init.lua
@@ -4,3 +4,11 @@ vim.cmd('colorscheme catppuccin')
 
 require("carson.opt")
 require("carson.keymap")
+
+vim.api.nvim_create_user_command("ProjCwd", function()
+	local root = vim.fs.root(0, {".git"})
+	if not root then
+		return vim.notify("no project root found", vim.log.levels.WARN)
+	end
+	vim.cmd.cd(root)
+end, { desc = "change cwd to project root" })
