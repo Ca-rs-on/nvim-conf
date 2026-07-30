7707722b3b99ced22002f713bf29ee1c44a27a8b
carson <carson.chambers@devrygreenhouses.com>
2026-07-30 08:00:19 -0700

    ctrl f is better used for paging. want to grep search within all sometimes too, keep it consistent. thanks tj deevs for small terminal command

--- lua/carson/keymap.lua
+++ lua/carson/keymap.lua
@@ -47,3 +47,11 @@ vim.keymap.set("n", "<A-k>", ":move -2<CR>==", { noremap = true, silent = true }
 
 vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- TJ Deevs!
+vim.keymap.set("n", "<leader>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0,15)
+end)
